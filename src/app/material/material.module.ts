b9bc82ddc17b7ea74b670692b8d8d7dc3ebad53d
Usman Ali <usmanali4073@gmail.com>
2019-06-30 17:08:46 -0400

    Navbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from "@angular/core";
 
-import { MatButtonModule, MatButtonToggleModule, MatIconModule, MatBadgeModule, MatProgressSpinnerModule } from '@angular/material';
-const MaterialComponents = [MatButtonModule, MatButtonToggleModule, MatBadgeModule, MatProgressSpinnerModule  ];
+import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule
+} from "@angular/material";
+
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule
+];
+
 @NgModule({
   declarations: [],
   imports: [MaterialComponents],
