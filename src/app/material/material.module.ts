eb0605272b6845a8929b359a378812c61d4bd780
Usman Ali <usmanali4073@gmail.com>
2019-07-02 00:45:51 -0400

    Grid List

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -10,7 +10,8 @@ import {
   MatSidenavModule,
   MatMenuModule,
   MatListModule,
-  MatDividerModule
+  MatDividerModule,
+  MatGridListModule
 } from "@angular/material";
 
 
@@ -24,7 +25,8 @@ const MaterialComponents = [
   MatMenuModule,
   MatIconModule,
   MatListModule,
-  MatDividerModule
+  MatDividerModule,
+  MatGridListModule
 ];
 
 @NgModule({
